832b825487576660400629cb826e0ca8ee322e26
mcesal <matej.cesal@gmail.com>
2026-08-16 11:08:41 +0200

    version adjustment

--- src/opentyrian_version.h
+++ src/opentyrian_version.h
@@ -2,7 +2,7 @@
 #define OPENTYRIAN_VERSION_H
 
 #ifndef OPENTYRIAN_VERSION
-#define OPENTYRIAN_VERSION "Engaged v1.3.2"
+#define OPENTYRIAN_VERSION "Engaged v1.3.1"
 #endif
 
 #ifndef OPENTYRIAN_COMMIT
