fe34ce9a14b6e2235ed9a08ea7620a9e42fd0939
mcesal <matej.cesal@gmail.com>
2026-08-29 08:03:53 +0200

    version up

--- src/opentyrian_version.h
+++ src/opentyrian_version.h
@@ -2,7 +2,7 @@
 #define OPENTYRIAN_VERSION_H
 
 #ifndef OPENTYRIAN_VERSION
-#define OPENTYRIAN_VERSION "Engaged v1.4.0"
+#define OPENTYRIAN_VERSION "Engaged v1.4.1"
 #endif
 
 #ifndef OPENTYRIAN_COMMIT
